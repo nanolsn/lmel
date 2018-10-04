68633c93d612ab0358cfc3779f3fb5af25b11b2e
nekosora <thenekosora@gmail.com>
2018-10-04 22:44:48 +0700

    matrix operations

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,11 +1,159 @@
 #include "Matrix.h"
 
-using namespace lmel;
 
-Matrix::Matrix()
+template<typename T>
+lmel::Matrix<T>::Matrix(T init)
+	: mat
+	{
+		init, init, init, init,
+		init, init, init, init,
+		init, init, init, init,
+		init, init, init, init
+	}
 {
 }
 
-Matrix::~Matrix()
+template<typename T>
+lmel::Matrix<T>::Matrix(const Matrix<T> & ref)
+{
+	for (unsigned i = 0; i < size; ++i)
+		mat[i] = ref[i];
+}
+
+template<typename T>
+lmel::Matrix<T>::~Matrix()
 {
 }
+
+template<typename T>
+lmel::Matrix<T> & lmel::Matrix<T>::operator=(const lmel::Matrix<T> & val)
+{
+	if (&val == this)
+		return *this;
+
+	for (unsigned i = 0; i < size; ++i)
+		mat[i] = val[i];
+
+	return *this;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator+(const lmel::Matrix<T> & val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] + val[i];
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator-(const lmel::Matrix<T> & val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] - val[i];
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator*(const lmel::Matrix<T> & val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] * val[i];
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> & lmel::Matrix<T>::operator+=(const lmel::Matrix<T> & val)
+{
+	for (unsigned i = 0; i < size; ++i)
+		mat[i] += val[i];
+
+	return *this;
+}
+
+template<typename T>
+lmel::Matrix<T> & lmel::Matrix<T>::operator-=(const lmel::Matrix<T> & val)
+{
+	for (unsigned i = 0; i < size; ++i)
+		mat[i] -= val[i];
+
+	return *this;
+}
+
+template<typename T>
+lmel::Matrix<T> & lmel::Matrix<T>::operator*=(const lmel::Matrix<T> & val)
+{
+	for (unsigned i = 0; i < size; ++i)
+		mat[i] *= val[i];
+
+	return *this;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator+(T val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] + val;
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator-(T val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] - val;
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator*(T val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] * val;
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::operator/(T val)
+{
+	Matrix result(0.0);
+
+	for (unsigned i = 0; i < size; ++i)
+		result[i] = mat[i] / val;
+
+	return result;
+}
+
+template<typename T>
+lmel::Matrix<T> lmel::Matrix<T>::transpose()
+{
+	Matrix tmp(this);
+
+	for (unsigned i = 0; i < rows; ++i)
+		for (unsigned j = 0; j < cols; ++j)
+			mat[i * cols + j] = tmp.mat[j * cols + i];
+}
+
+template<typename T>
+T lmel::Matrix<T>::get(unsigned n)
+{
+	return mat[n];
+}
+
--- main.cpp
+++ main.cpp
@@ -3,9 +3,57 @@
 
 using namespace lmel;
 
+template <typename T>
+void show(const Matrix<T> & mat)
+{
+	for (unsigned i = 0; i < mat.rows; ++i) {
+		for (unsigned j = 0; j < mat.cols; ++j)
+			std::cout << mat.get(i * mat.cols + j) << " ";
+
+		std::cout << "\n";
+	}
+}
+
 int main()
 {
-	std::cout << "LMEL Tests:\n";
+	Matrix<int> mat(0);
+	Matrix<int> mat0(0);
 
+	std::cout << (mat == mat0 ? "EQ!\n" : "NEQ!\n");
+
+	mat(0, 0) = 1;
+	mat(1, 0) = 1;
+	mat(2, 0) = 3;
+	mat(3, 0) = 1;
+
+	mat(3, 3) = 4;
+
+	show(mat);
+
+	std::cout << "\n";
+
+	mat.transpose();
+	Matrix<int> mat2 = mat * 2;
+
+	show(mat2);
+
+	std::cout << (mat == mat2 ? "\nEQ!\n" : "\nNEQ!\n");
+
+	mat = mat + mat;
+	show(mat);
+
+	std::cout << "\n";
+
+	mat += mat;
+	show(mat);
+
+	std::cout << "\n";
+
+	mat *= mat2;
+	show(mat);
+
+	std::cout << "\n";
+
+	getchar();
 	return 0;
 }
